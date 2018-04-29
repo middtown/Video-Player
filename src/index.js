26fa47f9f6c364030ace689c98ee2733538db0e4
Aaron Middleton <aarontm9@gmail.com>
2018-04-29 17:26:51 -0600

    working on search

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/search-bar';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const App = () => {
+	return (
+		<div>
+			<SearchBar />
+		</div>
+		);
+}
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+ReactDOM.render(<App />, document.querySelector('.container'));
